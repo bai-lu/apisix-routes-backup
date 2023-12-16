b7276c9c6f497d22fc4b7cf9058fc8a71e5c62aa
bailu <vasterlu@gmail.com>
2023-12-16 15:09:22 +0800

    init

--- readme.md
+++ readme.md
@@ -2,4 +2,17 @@
 
 从 apisix dashboard 获取路由列表, 保存之 routes.json, 执行脚本, 修改域名, 认证等参数进行恢复
 
-add a line
+仅恢复路由, 不支持恢复其他配置(如证书, 插件, 服务)
+
+## 指定恢复的 apisix 及 恢复路由文件
+
+创建环境变量
+
+```
+export APISIX_ADMIN_URL="https://ad.example.com/apisix/admin/routes"
+export ROUTES_BACKUP_FILE="routes.json"
+```
+
+## headers 获取
+
+部分访问 apisix-dashboards 从 chrome 开发者模式复制(fetch格式)
--- restore.py
+++ restore.py
@@ -13,7 +13,19 @@ with open(routes_backup_file) as f:
 
 
 # 从 chrome dev tools 复制
-headers = {}
+headers = {
+    "accept": "*/*",
+    "accept-language": "zh-CN,zh;q=0.9",
+    "authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDI3MTM1NzQsImlhdCI6MTcwMjcwOTk3NCwic3ViIjoiYmFpbHUifQ.mRY84K9yokwqVbz-_bPugokclm4sY-bVVdI1ENjJeuM",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin"
+}
 
 
 # 获取并打印 rows 对象
